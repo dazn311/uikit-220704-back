1f2e00b6b87a5b1b319a35cb9399b1d2f59e3dc7
Alexandr Ryzhenkov <alex2505@bk.ru>
2025-08-03 12:23:22 +0300

    upd PHPSESSID;

--- init.php
+++ init.php
@@ -1,9 +1,8 @@
 <?php
 // ini_set("session.cookie_secure", 1);
-$cookieParams = session_get_cookie_params();
-$cookieParams['samesite'] = 'None'; // or 'Strict', or 'None', or 'Lax'
-session_set_cookie_params($cookieParams);
+// $cookieParams = session_get_cookie_params();
 session_start();
+header('Set-Cookie: PHPSESSID= ' . session_id() . '; SameSite=None; Secure');
 require_once 'vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
