44c400d61c4df463c1a5224d5e9d10df537eb4b1
Alexandr Ryzhenkov <alex2505@bk.ru>
2025-08-04 11:05:05 +0300

    opt response-header;

--- pages/controller-user-info.php
+++ pages/controller-user-info.php
@@ -6,5 +6,4 @@ $knowledgeCode = "user-info.json";
 $ts='Kramp';
 $currentKnowledge = Articles::getArticle($ts, $knowledgeCode);
 
-// header("Content-Type: application/json");
 echo json_encode($currentKnowledge);
--- pages/response-header.php
+++ pages/response-header.php
@@ -1,9 +1,11 @@
 <?php
 
-header("Access-Control-Allow-Origin: https://localhost:3000"); // Разрешить все домены
-// header("Access-Control-Allow-Origin: *");
+// header("Access-Control-Allow-Origin: https://localhost:3000"); // Разрешить все домены
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Credentials: true");//Чтобы разрешить отправку куки с разных источников
 // header("Access-Control-Allow-Headers: content-type,cache-control, X-Requested-With");
 header("Access-Control-Allow-Headers: *");
+header("Access-Control-Max-Age: 3600");
+// header('Content-Type: application/json, text/plain');
 header('Content-Type: application/json; charset=utf-8');
